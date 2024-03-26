4698ea2f9f675d0bb3d3692cabfaa43514003324
llucaspprez <lucasprez@hotmail.com>
2024-03-26 23:32:35 +0100

    3

--- ponce_prez_b9/main.cpp
+++ ponce_prez_b9/main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+//Ejercicio 9: Escriba un programa que calcule el valor de: 1 * 2 * 3 *...* n (factorial)
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n;
+    unsigned long long factorial = 1; //unsigned es para que tome solo valores negativos y long porque es un numero grande
+    cout << "Ingrese un número positivo" << endl;
+    cin >> n;
+        if (n < 0) {
+        cout << "El factorial no esta definido para números negativos." << endl;
+    } else {
+        // con un for calculamos el factorial
+        for (int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        cout << "El factorial de " << n << " es: " << factorial << endl;
+    }
+
     return 0;
 }
