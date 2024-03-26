ef6b6eec3a3afa21d9aeb036e26c8795290d0e1f
llucaspprez <lucasprez@hotmail.com>
2024-03-26 22:36:35 +0100

    1

--- ponce_prez_a1/main.cpp
+++ ponce_prez_a1/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
-
 using namespace std;
+
+//Ejercicio 1:
+//A) Haga un programa que permita la entrada de datos, ingresando el nombre y apellido del usuario, además
+//debe mostrar el nombre_completo por pantalla. (considera los espacios)
+//B) Ejemplifique, como se coloca un comentario de una o varias líneas.
+//C) Como se baja un renglón al mostrar algo por pantalla, nombre alternativas.
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string nombre, apellido;
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+    cout << "Ingrese su apellido: ";
+    cin >> apellido;
+    string nombre_completo = nombre + " " + apellido;
+    cout << "El nombre completo es: "<< nombre_completo << endl;
+
+    // B) los comentarios se deben colocar entre "/*" y "*/" o con "//". El primero se usa para varias lineas y segundo solo una linea.
+
+    // ejemplo
+
+    /*
+    cout << "Ingrese su apellido: ";
+    cin >> apellido;
+    */
+
+    //C) Opciones: endl, '\n' son para bajar un renglón
     return 0;
 }
--- ponce_prez_a9/main.cpp
+++ ponce_prez_a9/main.cpp
@@ -2,8 +2,37 @@
 
 using namespace std;
 
+//Ejercicio 9: Escriba un programa que ingrese nombre y tiempo de nado de dos nadadoras. Luego determine y
+//muestre cuál de ellas llego primero.
+//
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    string nombre1;
+    int tiempoDeNado1;
+    string nombre2;
+    int tiempoDeNado2;
+    cout << "Ingrese el nombre del primer nadador: " << endl;
+    cin >> nombre1;
+    cout << "ingrese su tiempo: ";
+    cin >> tiempoDeNado1;
+    cout << "Ingrese el nombre del segundo nadador: " << endl;
+    cin >> nombre2;
+    cout << "Ingrese el tiempo: ";
+    cin >> tiempoDeNado2;
+
+    if(tiempoDeNado1 > tiempoDeNado2){
+        cout << "Llego primero " + nombre1 << endl;
+    }
+    if (tiempoDeNado1 < tiempoDeNado2) {
+        cout << "Llego primero " + nombre2 << endl;
+    }
+    if (tiempoDeNado1 == tiempoDeNado2)  {
+        cout << "Empataron" << endl;
+    }
+
+
     return 0;
 }
