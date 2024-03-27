2a088a05fd8e5d4d5e9d3a87e83502fddbcbf2fd
llucaspprez <lucasprez@hotmail.com>
2024-03-27 17:49:16 +0100

    Na

--- ponce_prez_b13/main.cpp
+++ ponce_prez_b13/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
 using namespace std;
+//
+//Ejercicio 13: Hacer un programa que muestre la serie Fibonacci, mostrando sus elementos desde el 1..hasta n. Ej.: si
+//n=13, nos muestra la salida: Fibonacci(13)= 1 1 2 3 5 8 13
+
 
 int main()
 {
-    cout << "ponce_prez_a1" << endl;
+    int n, a, b, c;
+    // este n es la cantidad de veces que ser se ejecutara la el ciclo for.
+     a = 0; // "a" es el primero número de la serie.
+     b = 1; // "b" es el segundo número de la serie.
+            // "c" es el que va acumulado las sumas.
+    cout << "Ingrese el número hasta donde quiere que se realice la serie" << endl;
+    cin >> n;
+    for (int i=0; i<=n; i++){
+         cout << a << " ";
+        c= a + b;
+        a= b;
+        b= c;
+    }
     return 0;
 }
--- ponce_prez_b3/main.cpp
+++ ponce_prez_b3/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+//Ejercicios 3: Realice un programa que calcule y muestre en la salida estándar, la suma de los cuadrados de los 10
+//primeros enteros mayores que cero. Mostrar un mensaje de invalidez, si ingresa un negativo.
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int suma = 0;
+
+    for (int i =1; i<=10; ++i){
+        suma = suma + i*i;
+    }
+
+    // como son los 10 promeros enteneros de mayores que 0 entonces sean siempre positivo.Ádemas de eso los elevas al cuadrado, siendo otra razón de que no pueden ser negativos.
+    std::cout << "La suma de los cuadrados de los 10 primeros enteros es: " << suma << std::endl;
+
+
     return 0;
 }
