b957b2d4f793a1e6222311039c0b0243efbf9d0f
Bárbara Ponce <barbaraponce93@gmail.com>
2024-04-02 19:27:56 -0300

    b todo

--- ponce_prez_b10/main.cpp
+++ ponce_prez_b10/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 
 using namespace std;
+/* 10: Escriba un programa que calcule el valor de:
+ 1! +2! + 3! + ... + n! (suma de factoriales)*/
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int num,suma=1;
+    cout << "Ingrese el numero para la suma de factoriales" << endl;
+    cin>> num
+
+    if(num==0 || num==1)
+    {
+        return 1;
+    }
+    else
+    {
+        for(int i=1; i<=num; i++)
+        {
+
+            suma*=i;
+
+        }
+         cout << "El factorial de " << num << " es: " << suma << endl;
+    }
+
     return 0;
 }
--- ponce_prez_b6/main.cpp
+++ ponce_prez_b6/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-
+#include <stdlib.h>
+#include <cmath>
 using namespace std;
-
+/*Ejercicio 6: Escriba un programa que calcule num^exp, donde tanto num
+como exp son enteros positivos, sin utilizar la función pow.
+ Donde num será una base generada de forma aleatoria entre 2 y 42,
+  y exp será ingresada por teclado nomayor a 5.
+  Usar las librerías stdlib.h y cmath.
+*/
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    int base = rand() % 41 + 2;//num aleatorio
+    int exponente,multiplica=1;
+    cout<<"Ingrese el exponente (menor o igual a 5): ";
+    cin>>exponente;
+
+    if (exponente > 5)
+    {
+        printf("El exponente debe ser mayor a 5.\n");
+        return 1;// sin este return muestra el mensaje del if  pero tambien hace la cuenta
+    }
+
+    for(int i=0; i<exponente; i++) //la cantidad de multiplicacioes segun el expo
+    {
+
+        multiplica*=base;
+    }
+
+    cout<< base <<" elevado a la " <<exponente<<" es igual a: "<<multiplica<<endl;
+
+//
+
     return 0;
 }
